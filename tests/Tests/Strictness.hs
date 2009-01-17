063c18ae77b696847cb6973d36151e0bcf310be8
Matti Niemenmaa <matti.niemenmaa+git@iki.fi>
2009-01-17 16:28:12 +0200

    New strictness tests: insertWith vs. insertWith'.

--- tests/Tests/Strictness.hs
+++ tests/Tests/Strictness.hs
@@ -1,9 +1,61 @@
 -- File created: 2009-01-06 13:08:00
 
+{-# LANGUAGE CPP, TemplateHaskell #-}
+
 module Tests.Strictness (tests) where
 
-import Test.Framework
-import Test.Framework.Providers.HUnit
-import Test.HUnit.Base
+import Test.ChasingBottoms.IsBottom   (isBottom)
+import Test.Framework                 (testGroup)
+import Test.Framework.Providers.HUnit (testCase)
+import Test.HUnit                     (assert)
 
-tests = testGroup "Strictness" []
+import qualified Data.Trie.Set.Eq
+import qualified Data.Trie.Set.Ord
+import qualified Data.Trie.Set.Enum
+import qualified Data.Trie.Map.Eq
+import qualified Data.Trie.Map.Ord
+import qualified Data.Trie.Map.Enum
+import qualified Data.Trie.Patricia.Set.Eq
+import qualified Data.Trie.Patricia.Set.Ord
+import qualified Data.Trie.Patricia.Set.Enum
+import qualified Data.Trie.Patricia.Map.Eq
+import qualified Data.Trie.Patricia.Map.Ord
+import qualified Data.Trie.Patricia.Map.Enum
+
+import Tests.Base
+import Tests.TH
+
+-- size doesn't evaluate the values but it does traverse the whole trie
+-- returning a single result, so it works well for checking whether there are
+-- any bottoms in the trie
+#define IS_LAZY   (not.isBottom.size)
+#define IS_STRICT (    isBottom.size)
+
+-- insertWith' should apply the combining function strictly, insertWith should
+-- not. We use a singleton of [] to make sure that the combining function is
+-- called.
+$(makeFunc mapsOnly ["size","singleton","insertWith"] [d|
+   insertWith size singleton insertWith =
+      IS_LAZY . insertWith undefined [] undefined $
+         (singleton [] 0 :: TrieType)
+ |])
+$(makeFunc mapsOnly ["size","singleton","insertWith'"] [d|
+   insertWith'1 size singleton insertWith' =
+      IS_STRICT . insertWith' undefined [] undefined $
+         (singleton [] 0 :: TrieType)
+ |])
+$(makeFunc mapsOnly ["size","singleton","insertWith'"] [d|
+   insertWith'2 size singleton insertWith' =
+      IS_STRICT . insertWith' (+) [] undefined $ (singleton [] 0 :: TrieType)
+ |])
+$(makeFunc mapsOnly ["size","singleton","insertWith'"] [d|
+   insertWith'3 size singleton insertWith' =
+      IS_STRICT . insertWith' undefined [] 0 $ (singleton [] 0 :: TrieType)
+ |])
+
+tests = testGroup "Strictness"
+   [ $(makeCases mapsOnly "insertWith")
+   , $(makeCases mapsOnly "insertWith'1")
+   , $(makeCases mapsOnly "insertWith'2")
+   , $(makeCases mapsOnly "insertWith'3")
+   ]
